482a5b4c22d654a30f85083af77f89eeb2e965e6
rasel00930 <raselahmed00930@gmail.com>
2025-02-01 01:02:20 +0600

    This is Main Commit

---
+++ Backend/backend-dotnet7/Core/Dtos/Auth/MeDto.cs
@@ -0,0 +1,6 @@
+﻿namespace backend_dotnet7.Core.Dtos.Auth;
+
+public class MeDto
+{
+    public string Token { get; set; }
+}
---
+++ Backend/backend-dotnet7/Core/Dtos/Message/CreateMessageDto.cs
@@ -0,0 +1,7 @@
+﻿namespace backend_dotnet7.Core.Dtos.Message;
+
+public class CreateMessageDto
+{
+    public string ReceiverUserName { get; set; }
+    public string Text { get; set; }
+}
